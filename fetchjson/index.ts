4d044398c183c168983e02a1a6313fd75acfb463
Genilton Souza <genilton.souza26@gmail.com>
2020-09-24 22:19:36 -0400

    lesson 8

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,20 +1,24 @@
 import axios from "axios";
 const url = "http://jsonplaceholder.typicode.com/todos/1";
 interface Todo {
- id: number;
- title: string;
- completed: boolean;
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
   const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The todo with ${id} 
     Has a title pf: ${title}
-    Is it finished? ${finished}
-  `)
-});
+    Is it finished? ${completed}
+  `);
+};
